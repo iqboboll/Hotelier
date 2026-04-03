f80691f9fbfe1f4a4ad7e35a8bd63402afd18a8f
iqboboll <qbal0876@gmail.com>
2026-04-03 23:18:02 +0800

    feat: add database connection configuration using PDO for the API

--- api/db.php
+++ api/db.php
@@ -9,9 +9,9 @@ $charset = 'utf8mb4';
 
 $dsn = "mysql:host=$host;dbname=$dbname;charset=$charset";
 $options = [
-    PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
     PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-    PDO::ATTR_EMULATE_PREPARES   => false,
+    PDO::ATTR_EMULATE_PREPARES => false,
 ];
 
 try {
